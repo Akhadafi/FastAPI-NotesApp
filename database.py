ec4ca63bd802a2e0616b3f41e3e85849478f59b6
Akhadafi <khadafi.dev@gmail.com>
2022-12-05 10:56:09 +0700

    Connect to HarperDB

---
+++ database.py
@@ -0,0 +1,9 @@
+from secrets_1 import HARPERBD_PASSWORD, HARPERBD_URL, HARPERBD_USERNAME
+
+import harperdb
+
+db = harperdb.HarperDB(
+    url=HARPERBD_URL,
+    username=HARPERBD_USERNAME,
+    password=HARPERBD_PASSWORD,
+)
