d1d6b871eeefcd14a837842126f7da698b58eba2
Akhadafi <khadafi.dev@gmail.com>
2022-12-05 19:46:23 +0700

    Post Put Data

--- frontend/src/pages/Note.jsx
+++ frontend/src/pages/Note.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 
 const Note = ({ match }) => {
   let params = useParams();
+  let navigate = useNavigate();
   let noteId = params.id;
 
   // let foundNote = dummyData.find((note) => note.id === noteId);
@@ -19,18 +20,38 @@ const Note = ({ match }) => {
     setNote(data);
   };
 
+  let submitData = async () => {
+    let url = 'http://127.0.0.1:8000/notes';
+    let method = 'POST';
+
+    if (noteId !== 'add') {
+      url = `http://127.0.0.1:8000/notes/${noteId}`;
+      method = 'PUT';
+    }
+
+    await fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ body: note.body }),
+    });
+
+    navigate('/');
+  };
+
   return (
     <div>
       <Link to={'/'}>Go Back</Link>
       {noteId !== 'add' && <button>Delete</button>}
       <textarea
         onChange={(e) => {
-          setNote(e.target.value);
+          setNote({ ...note, body: e.target.value });
         }}
         value={note?.body}
         placeholder='Enter Note...'
       ></textarea>
-      <button>Save</button>
+      <button onClick={submitData}>Save</button>
     </div>
   );
 };
