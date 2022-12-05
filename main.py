669392076e67396b61f4c5869fd6d3e300d1f78b
Akhadafi <khadafi.dev@gmail.com>
2022-12-05 11:06:05 +0700

    Get Note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from database import db
 
 app = FastAPI()
 
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 def getRoutes():
     return ["/notes", "notes/<pk>"]
+
+
+@app.get("/notes")
+def getNotes():
+    # notes = db.search_by_value("notesapp", "notes", "id", "*", get_attributes=["*"])
+    notes = db.sql("SELECT * FROM notesapp.notes ORDER BY __updatedtime__ DESC")
+    return notes
