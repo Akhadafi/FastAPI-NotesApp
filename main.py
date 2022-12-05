b4e6f5dc52ae0e56ca8bec3f792659264a6af5e8
Akhadafi <khadafi.dev@gmail.com>
2022-12-05 11:13:30 +0700

    Add Notes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from database import db
 
 app = FastAPI()
@@ -13,4 +13,11 @@ def getRoutes():
 def getNotes():
     # notes = db.search_by_value("notesapp", "notes", "id", "*", get_attributes=["*"])
     notes = db.sql("SELECT * FROM notesapp.notes ORDER BY __updatedtime__ DESC")
+    return notes
+
+
+@app.post("/notes")
+def addNotes(data=Body()):
+    db.insert("notesapp", "notes", [{"body": data["body"]}])
+    notes = db.search_by_value("notesapp", "notes", "id", "*", get_attributes=["*"])
     return notes
